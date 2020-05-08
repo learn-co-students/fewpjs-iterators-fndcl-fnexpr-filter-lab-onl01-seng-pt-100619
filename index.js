f21cb91a013a8bfd818b91df28d722af09031f96
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-08 14:37:08 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    let list = drivers.filter(driver => { return string.toLowerCase() === driver.toLowerCase()})
+    return list; // filter can be summoned with the arrow notation
+}
+
+function fuzzyMatch(drivers, string){
+    let list = drivers.filter(driver => { return string[0] === driver[0]})
+    return list; // we assign the filter method to a variable to access the returned result as an array
+}
+
+function matchName(drivers, string){
+    let list = drivers.filter(driver => { return string === driver.name})
+    return list; // we can access an object inside the array by .attribute
+}
+
